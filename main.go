20fe3ace7698864b39a881dd40674222e1773901
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-10-23 16:58:00 +0300

    rm

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"customType/pkg/custtype"
-	"fmt"
-)
-
 func main() {
-	builder := custtype.Builder{}
-	jsonStr := `{"value":"OFF"}`
 
-	counter32, err := builder.CreateCustomBool([]byte(jsonStr))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(counter32)
 }
